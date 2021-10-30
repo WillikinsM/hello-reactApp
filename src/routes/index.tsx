98640a02e6f1386e371b720beee6e2d3a7d89770
WillikinsM <willikins98@gmail.com>
2021-10-30 19:46:21 -0300

    fixed breadcrumbs component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,59 @@
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import Hello from "./hello";
 import DataTab from "./myData";
 import MyInfos from "./my-infos/Index";
-import Breadcrumb from "../components/breadcrumbs";
+import Breadcrumbs from "../components/breadcrumbs";
 
-const items = [
-  { to: "/", label: "Home" },
-  { to: "/table", label: "Table" },
-  { to: "/infos", label: "My-Infos" },
+const routes = [
+  { path: "/", label: "Home", Component: Hello },
+  { path: "/table", label: "Table", Component: DataTab },
+  { path: "/infos", label: "My-Infos", Component: MyInfos },
 ];
 
-const Routes = () => {
-  return (
-    <div className="route">
-      <Router>
-        <Breadcrumb>
-          {items.map(({ to, label }) => (
-            <Link key={to} to={to}>
-              {label}
-            </Link>
-          ))}
-        </Breadcrumb>
-        <Route component={Hello} path="/" exact />
-        <Route component={DataTab} path="/table" />
-        <Route component={MyInfos} path="/infos" />
-      </Router>
+const Routes = () => (
+  <Router>
+    <div className="router">
+      {routes.map(({ path, label, Component }, key) => (
+        <Route
+          exact
+          path={path}
+          key={key}
+          render={(props: any) => {
+            const crumbs = routes
+              .filter(({ path }) => props.match.path.includes(path))
+              .map(({ path, ...rest }) => ({
+                path: Object.keys(props.match.params).length
+                  ? Object.keys(props.match.params).reduce(
+                      (path, param) =>
+                        path.replace(`:${param}`, props.match.params[param]),
+                      path
+                    )
+                  : path,
+                ...rest,
+              }));
+
+            return (
+              <div className="bread">
+                <Breadcrumbs crumbs={crumbs} />
+                <Component {...props} />
+              </div>
+            );
+          }}
+        />
+      ))}
     </div>
-  );
-};
+  </Router>
+);
 export default Routes;
+
+// */<Breadcrumb>
+//           {routes.map(({ path, label }) => (
+//             <Link key={path} to={path}>
+//               {label}
+//             </Link>
+//           ))}
+//         </Breadcrumb>
+//         <Route component={Hello} path="/" exact />
+//         <Route component={DataTab} path="/table" />
+//         <Route component={MyInfos} path="/infos" />
+//           *
