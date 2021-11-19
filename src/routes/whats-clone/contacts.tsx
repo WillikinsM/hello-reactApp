68078decc12f4756abb6e743ef0dfd6a125fd83e
WillikinsM <willikins98@gmail.com>
2021-11-19 15:29:59 -0300

    implemented the search-bar ui

--- src/routes/whats-clone/contacts.tsx
+++ src/routes/whats-clone/contacts.tsx
@@ -22,20 +22,43 @@ const Contacts = () => {
             </span>
           </div>
         </header>
-        <div className="search-contacts">
-          <div className="input-group mb-3">
-            <span className="input-group-text" id="basic-addon1">
-              @
-            </span>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Username"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-            ></input>
+
+        <div className="search-wrapper">
+          <div className="search-icons">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              className="search-icon"
+            >
+              <path
+                fill="currentColor"
+                d="M15.009 13.805h-.636l-.22-.219a5.184 5.184 0 0 0 1.256-3.386 5.207 5.207 0 1 0-5.207 5.208 5.183 5.183 0 0 0 3.385-1.255l.221.22v.635l4.004 3.999 1.194-1.195-3.997-4.007zm-4.808 0a3.605 3.605 0 1 1 0-7.21 3.605 3.605 0 0 1 0 7.21z"
+              ></path>
+            </svg>
+
+            <div className="search__back-btn">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12 4l1.4 1.4L7.8 11H20v2H7.8l5.6 5.6L12 20l-8-8 8-8z"
+                ></path>
+              </svg>
+            </div>
           </div>
+
+          <input
+            className="input-search"
+            placeholder="Search or start a new chat"
+          ></input>
         </div>
+
         <div className="list-group list-group-flush border-bottom scrollarea">
           <a
             href="#"
